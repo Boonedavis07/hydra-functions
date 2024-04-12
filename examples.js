e8a95b2e9cc4450b9007a8b2968d36f34d0f5d91
Boonedavis07 <166757514+Boonedavis07@users.noreply.github.com>
2024-04-12 13:23:05 -0400

    Update osc examples 1&2

--- examples.js
+++ examples.js
@@ -46,7 +46,7 @@ noise( () => Math.sin(time/10)*50 , () => Math.sin(time/2)/500 )
    osc: {
       example: [
          {
-            code: `osc( [1,10,50,100,250,500].fast(2) ).out(o0)`,
+            code: `osc( 100 ).out(o0)`,
             comments: {
                en: "frequency",
                es: "frecuencia",
@@ -54,7 +54,7 @@ noise( () => Math.sin(time/10)*50 , () => Math.sin(time/2)/500 )
             }
          },
          {
-            code: `osc( () => Math.sin(time/10) * 100 ).out(o0)`,
+            code: `osc(8,0.2,2).kaleid().out(o0)`,
             comments: {
                en: "frequency 2",
                es: "frecuencia 2",
